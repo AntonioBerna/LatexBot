0797aad64ca634b94ebfaf664cbce8d350349495
Antonio Bernardini <60107274+AntonioBerna@users.noreply.github.com>
2020-10-29 18:07:57 +0100

    Update webscraping.py

--- webscraping.py
+++ webscraping.py
@@ -8,6 +8,7 @@ class Scraper:
     options = Options()
     options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     options.add_argument("--headless")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
 
     def __init__(self):
